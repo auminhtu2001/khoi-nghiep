af4e0eb0bc9c585342da3e7a55801fe42950041f
Slacker <tienduongeng2001@gmail.com>
2022-09-25 21:20:21 +0700

    Start

---
+++ QuanLyCayXang/QuanLyCayXang/Form1.cs
@@ -0,0 +1,15 @@
+namespace QuanLyCayXang
+{
+    public partial class FormDangNhap : Form
+    {
+        public FormDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
